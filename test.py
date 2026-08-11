271a4ecc6d56de9c19ba04fd1c97c9e8cbbb86cb
maorh22-S <maorh22@gmail.com>
2026-08-11 11:47:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -337,7 +337,7 @@ else:
         @media (min-width: 768px) {
         div[data-testid="stForm"] { max-width: 80% !important; width: 80% !important; margin: 0 auto !important; padding: 1.5rem !important; }
         div[data-testid="stAppViewBlockContainer"] { max-width: 85% !important; }
-        
+        div[data-testid="stHorizontalBlock"] { display: flex !important; flex-direction: row !important; flex-wrap: nowrap !important; gap: 8px !important; row-gap: 0px !important; align-items: start !important; }
         div[data-testid="stHorizontalBlock"] { gap: 8px !important; row-gap: 0px !important; align-items: start !important; }
         .pc-row-container { height: 95px !important; display: flex; flex-direction: column; justify-content: flex-start !important; margin-bottom: 0px !important; padding: 0px !important; }
         div[data-testid="stCheckbox"] { margin-top: 0px !important; margin-bottom: -12px !important; padding: 0px !important; height: 24px !important; }

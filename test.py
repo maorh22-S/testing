99a99441fb85da781c18c1608056bd74c560c2ed
maorh22-S <maorh22@gmail.com>
2026-09-16 08:01:39 +0300

    Fix typo in DISABLE_pilot assignment
    Corrected the value of DISABLE_pilot from True to Fulse.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import requests
 from datetime import datetime, timedelta, timezone
 
 # ⚙️ מפסק פיילוט: True הופך את (לילה, פתיחת בוקר כל השבוע וכל שישי-שבת) לאפורות. False פותח כרגיל.
-DISABLE_pilot = True
+DISABLE_pilot = Fulse
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 def check_shift_blocking(day_en, shift_en, current_role, disable_pilot_flag):
